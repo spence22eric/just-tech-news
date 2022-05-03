3471c4360d3644f5e63436ea0974436132ec4478
Eric Spencer <spencer22eric@gmail.com>
2022-05-02 22:08:56 -0400

    add comments routes and associations

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Vote, Comment } = require("../../models");
 
 // get all users
 router.get('/', (req, res) => {
@@ -16,10 +16,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
 	User.findOne({
 		attributes: { exclude: ['password'] },
+		where: {
+			id: req.params.id
+		},
 		include: [
 			{
 				model: Post,
 				attributes: ['id', 'title', 'post_url', 'created_at']
+			},
+			// include the Comment model here:
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'created_at'],
+				include: {
+					model: Post,
+					attributes: ['title']
+				}
 			},
 			{
 				model: Post,
@@ -27,10 +39,7 @@ router.get('/:id', (req, res) => {
 				through: Vote,
 				as: 'voted_posts'
 			}
-		],
-		where: {
-			id: req.params.id
-		}
+		]
 	})
 		.then(dbUserData => {
 			if (!dbUserData) {
