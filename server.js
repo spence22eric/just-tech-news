fd87d3a08a1e1647565426e5f5e7b04d166adcc2
Eric Spencer <spencer22eric@gmail.com>
2022-05-08 15:18:00 -0400

    add conditional rendering and comment functionality

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -18,24 +19,19 @@ const sess = {
   })
 };
 
+app.use(session(sess));
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
 
+app.use(require('./controllers/'));
 
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
